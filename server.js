055aa04641b634246697cf605388af2f1db6319b
Sam O'Cain <samocain93@gmail.com>
2023-02-01 17:24:44 -0600

    server js file set up

--- server.js
+++ server.js
@@ -12,5 +12,43 @@ const helpers = require('./utils/helpers');
 
 // Starting express app
 const app = express();
-const PORT = 
+const PORT = process.env.PORT || 3001;
+
+// SET UP SESSIONS
+
+const sess = {
+    secret: 'Top secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+// Init express session
+app.use(session(sess));
+
+
+const hbs = express.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({
+    force: false
+})
+.then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+})
+
+
 
