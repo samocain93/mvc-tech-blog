2258b186ac401c3a43a8a0e3124eaf15c0df6299
Sam O'Cain <samocain93@gmail.com>
2023-02-08 10:06:55 -0600

    find all posts route added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,20 +14,59 @@ const withAuth = require('../utils/auth')
 //     }
 // })
 
+// Working route to render all posts to page
 
-router.get('/', (req, res) => {
-    Post.findAll({
-        include: [
-            {
-                model: User
+// router.get('/', (req, res) => {
+//     Post.findAll({
+//         include: [
+//             {
+//                 model: User
+//             }
+//         ]
+//     }).then((posts) => {
+//         console.log(posts);
+//         posts = posts.map((post) => post.get({plain: true}));
+//         res.render('homepage', { posts })
+//     })
+// })
+
+
+router.get('/', async (req, res) => {
+    const postData = await Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'date_created',
+        ],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'text', 'user_id', "post_id" ],
+            include: {
+                model: User,
+                attributes: ['username']
             }
-        ]
-    }).then((posts) => {
-        console.log(posts);
-        posts = posts.map((post) => post.get({plain: true}));
-        res.render('homepage', { posts })
+        }]
     })
-})
+    .then(postData => {
+        const posts = postData.map(post => post.get({ plain: true}));
+        console.log(posts);
+        res.render('homepage', { possts, loggedIn: req.session.loggedIn})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+
+
+
+
+
+
 
 
 
