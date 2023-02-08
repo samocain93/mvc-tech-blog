5cccc70ad3416f09231af33217f1fcd7653f5083
Sam O'Cain <samocain93@gmail.com>
2023-02-07 18:33:06 -0600

    some more handlebars work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,17 +1,33 @@
-const sequelize = require('../config/connection');
+
 const { Post, User, Comment} = require('../models');
 const router = require('express').Router();
+const withAuth = require('../utils/auth')
 
 
+// Working route to render public index html page for testing 
 
-router.get('/', async (req, res) => {
-    try {
-        await res.render('./public/index.html')
-    } catch (err) {
-        res.status(500).json(err)
-    }
+// router.get('/', async (req, res) => {
+//     try {
+//         await res.render('./public/index.html')
+//     } catch (err) {
+//         res.status(500).json(err)
+//     }
+// })
+
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        include: [
+            {
+                model: User
+            }
+        ]
+    }).then((posts) => {
+        console.log(posts);
+        posts = posts.map((post) => post.get({plain: true}));
+        res.render('homepage', { posts })
+    })
 })
-
 
 
 
