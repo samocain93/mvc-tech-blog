837fe4670cd619d612322bf5a1c81974a90ed1b1
Sam O'Cain <samocain93@gmail.com>
2023-02-08 10:19:54 -0600

    added get route for single post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,6 +14,7 @@ const withAuth = require('../utils/auth')
 //     }
 // })
 
+
 // Working route to render all posts to page
 
 // router.get('/', (req, res) => {
@@ -31,6 +32,7 @@ const withAuth = require('../utils/auth')
 // })
 
 
+// Get route for finding all Posts and rendering to page
 router.get('/', async (req, res) => {
     const postData = await Post.findAll({
         attributes: [
@@ -61,8 +63,60 @@ router.get('/', async (req, res) => {
 
 
 
+// LOGIN 
+// If login is clicked, redirect to login
+// Otherwise, if logged in, redirect home
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login')
+});
 
 
+
+
+// Route to view single post 
+// This will be when a user clicks on a post on the home page
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'date_created',
+        ],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'text', 'user_id', 'post_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }]
+    })
+    .then(postData => {
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+        }
+        const post = postData.get({ plain: true });
+        console.log(post);
+        res.render('post', { post, loggedIn: req.session.loggedIn})
+    })
+
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+ 
+})
 
 
 
--- models/Comment.js
+++ models/Comment.js
@@ -29,6 +29,7 @@ Comment.init(
                 key: 'id',
             },
         },
+        
 
     },
     {
