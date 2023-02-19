0e719a6f0c036ad8cbb61d65156480cf2feccb16
Sam O'Cain <samocain93@gmail.com>
2023-02-18 21:29:13 -0600

    login form handlers added

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,39 @@
+const signupButton = document.getElementById("signup-button");
+
 const loginFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Get values from login form
-    const username = document.getElementById('username-login');
-    const password = document.getElementById('password-login');
+  // Get values from login form
+  const username = document
+    .getElementById("username-login")
+    .ariaValueMax.trim();
+  const password = document
+    .getElementById("password-login")
+    .ariaValueMax.trim();
 
-    if (username && password) {
-        // Send POST request to the API endpoint
-        const response = await fetch('/api/users',{
-            method: 'POST',
-            body: JSON.stringify({ email, password }),
-            headers: { 'Content-Type': 'application/json'}
-        });
+  if (username && password) {
+    // Send POST request to the API endpoint
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok) {
-            // If successful, redirect the browser to the profile page
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
     }
-}
+  }
+};
+
+const signupTransfer = function () {
+  document.location.replace("/signup");
+};
+
+signupButton.addEventListener("click", signupTransfer);
+
+document
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,17 +1,32 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.getElementById('username-signup').ariaValueMax.trim();
-    const password = document.getElementById('signup-password').ariaValueMax.trim();
+  const username = document
+    .getElementById("username-signup")
+    .ariaValueMax.trim();
+  const password = document
+    .getElementById("signup-password")
+    .ariaValueMax.trim();
 
-    if (username && password) {
-        const response = await fetch('./api/users', {
-            method: 'POST',
-            body: JSON.stringify({
-                username,
-                password
-            })
-        })
+  if (username && password) {
+    const response = await fetch("./api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      console.log("Sign up succesful");
+
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
     }
+  }
+};
 
-}
+document
+  .querySelector("#signup-form")
+  .addEventListener("submit", signupFormHandler);
