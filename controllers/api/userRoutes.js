bb91d43d4b4c6e643836acaf9b4def439f605af2
Sam O'Cain <samocain93@gmail.com>
2023-02-19 20:49:31 -0600

    user routes done

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,15 +1,61 @@
-const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
 
-// Get router for api/users
-// Returning all users info minus password
-router.get('/', (req, res) => {
-    User.findAll({ 
-        attributes: {exclude: ['[password']}
-    })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.loggedIn = true;
+
+      res.status(200).json(userData);
     });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { email: req.body.email },
+    });
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again." });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again." });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.loggedIn = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    res.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
