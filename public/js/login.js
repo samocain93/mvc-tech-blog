846169a87a4f007e7242c6431532fb4d27f913c4
Sam O'Cain <samocain93@gmail.com>
2023-02-23 15:34:10 -0600

    reworking some user routes to fix login

--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,24 @@
-const signupButton = document.getElementById("signup-button");
-
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  // Get values from login form
-  const username = document
-    .getElementById("username-login")
-    .ariaValueMax.trim();
-  const password = document
-    .getElementById("password-login")
-    .ariaValueMax.trim();
+  const username = document.getElementById('username-login').value.trim();
+  const password = document.getElementById('password-login').value.trim();
 
   if (username && password) {
-    // Send POST request to the API endpoint
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace("/dashboard");
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
   }
 };
 
-const signupTransfer = function () {
-  document.location.replace("/signup");
-};
-
-signupButton.addEventListener("click", signupTransfer);
-
 document
-  .querySelector("#login-form")
-  .addEventListener("submit", loginFormHandler);
+  .getElementById('login-form')
+  .addEventListener('submit', loginFormHandler);
